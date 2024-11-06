a173ba6b38dd069fa658ad26274169e1e1938495
joshua <joshua@vaticle.com>
2024-11-06 11:55:55 -0500

    Push scripts and README

---
+++ sample_flamegraph_mac.sh
@@ -0,0 +1,17 @@
+pid=$1
+duration=$2
+timestamp=$(date +%s)
+interval_ms=1
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+data_dir=$SCRIPT_DIR/results/$timestamp
+
+mkdir -p $data_dir
+
+sample $pid -f $data_dir/samples.prof $duration $interval_ms 
+filtercalltree $data_dir/samples.prof > $data_dir/samples_filtered.prof
+$SCRIPT_DIR/stackcollapse-sample.awk $data_dir/samples_filtered.prof > $data_dir/calls.folded
+$SCRIPT_DIR/FlameGraph/flamegraph.pl $data_dir/calls.folded > $data_dir/flamegraph.svg
+
+echo Result: $data_dir/flamegraph.svg
+
