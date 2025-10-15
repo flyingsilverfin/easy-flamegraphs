21d63c6ee8e3eccacfa713867a19b9e230de9923
joshua <joshua@typedb.com>
2025-10-15 14:43:18 -0400

    Fixes

--- sample_flamegraph_linux_perf.sh
+++ sample_flamegraph_linux_perf.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 pid=$1
 duration=$2
 timestamp=$(date +%s)
@@ -13,7 +15,7 @@ mkdir -p $data_dir
 perf record -F $sampling_rate -p $pid -g -- sleep $duration
 perf script > $data_dir/samples.perf
 
-$SCRIPT_DIR/stackcollapse-perf.pl $data_dir/samples.perf > $data_dir/samples.folded
+$SCRIPT_DIR/FlameGraph/stackcollapse-perf.pl $data_dir/samples.perf > $data_dir/samples.folded
 $SCRIPT_DIR/FlameGraph/flamegraph.pl $data_dir/samples.folded > $data_dir/flamegraph.svg
 
 echo Result: $data_dir/flamegraph.svg
